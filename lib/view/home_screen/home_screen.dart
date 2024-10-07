276c6db3e46362fa7a989a77ec7a9e915267f5e1
ABY4613 <abyb4613@gmail.com>
2024-10-07 16:52:20 +0530

    list updated

--- lib/view/home_screen/home_screen.dart
+++ lib/view/home_screen/home_screen.dart
@@ -18,7 +18,7 @@ class HomeScreen extends StatefulWidget {
 List dices =[
     " assets/images/d1.png",
      " assets/images/d2.png",
-     "assets/images/d3.png",
+    
      " assets/images/d4.png",
 
 ];
