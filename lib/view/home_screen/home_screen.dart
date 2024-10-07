f878abd837b1204a00338bb0cc3a7462678fe7f0
ABY4613 <abyb4613@gmail.com>
2024-10-07 16:53:01 +0530

    dice

--- lib/view/home_screen/home_screen.dart
+++ lib/view/home_screen/home_screen.dart
@@ -18,8 +18,11 @@ class HomeScreen extends StatefulWidget {
 List dices =[
     " assets/images/d1.png",
      " assets/images/d2.png",
-    
+     "assets/images/d3.png",
      " assets/images/d4.png",
+    "assets/images/d5.png",
+    "assets/images/d6.png",
+
 
 ];
 int? randomnumber ;
